10e694a4f10e6237d84d495fcf021aa805bd6782
Boothe-Cayden <caydenjohn@gmail.com>
2025-08-15 12:14:47 -0500

    Added the nav bar scss styles

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -42,7 +42,7 @@ export default class PortfolioContainer extends Component {
   portfolioItems() {
     return this.state.data.map(item => {
       console.log("item data", item);
-      return <PortfolioItem key={item.id} title={item.name} url={item.url} slug={item.name} />;
+      return <PortfolioItem key={item.id} item={item} />;
     });
   }
 
