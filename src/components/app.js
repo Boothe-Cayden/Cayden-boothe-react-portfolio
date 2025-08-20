09403539626e424578c3cc536706cea327a35cbb
Boothe-Cayden <caydenjohn@gmail.com>
2025-08-20 16:35:49 -0500

    converted navigation container from a class function to a standard function

--- src/components/app.js
+++ src/components/app.js
@@ -88,7 +88,7 @@ export default class App extends Component {
                 )}
               />
 
-              <Route path="/about-me" component={About} />
+              <Route path="/about" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
               <Route
