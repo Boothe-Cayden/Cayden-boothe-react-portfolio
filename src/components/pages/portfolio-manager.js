62c7b4755e332cab855e45b6ba978d91266b4971
Boothe-Cayden <caydenjohn@gmail.com>
2025-08-22 13:12:07 -0500

    added a portfolio form as well as the portfolio itmes to the right column of the portfolio manager page

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
-import PortfolioItem from "../portfolio/portfolio-item";
+
+import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
+import PortfolioForm from "../portfolio/portfolio-form";
 
 export default class PortfolioManager extends Component {
 
@@ -8,15 +10,30 @@ export default class PortfolioManager extends Component {
         super();
 
         this.state= {
-            PortfolioItems: []
+            portfolioItems: []
+        };
+
+        this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(
+            this
+          );
+          this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
         }
+      
+        handleSuccessfulFormSubmission(portfolioItem) {
+          // TODO
+          // update the portfolioItems state
+          // and add the portfolioItem to the list
+        }
+      
+        handleFormSubmissionError(error) {
+          console.log("handleFormSubmissionError error", error);
     }
 
     getPortfolioItems() {
         axios.get("https://caydenboothe.devcamp.space/portfolio/portfolio_items", { withCredentials: true })
         .then(response => {
             this.setState({
-                PortfolioItems: [...response.data.portfolio_items]
+                portfolioItems: [...response.data.portfolio_items]
             })
         }).catch(error => {
             console.log("error in getPortfolioItems", error)
@@ -31,11 +48,14 @@ export default class PortfolioManager extends Component {
             <div className="portfolio-manager-wrapper">
                 
                 <div className="left-column">
-                    <h1>Portfolio form</h1>
+                <PortfolioForm
+                    handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission}
+                    handleFormSubmissionError={this.handleFormSubmissionError}
+                />
                 </div>
 
                 <div className="right-column">
-                    <h1>portfolio sidebar...</h1>
+                    <PortfolioSidebarList data={this.state.portfolioItems} />
                 </div>
             </div>
         );
